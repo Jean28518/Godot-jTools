3aefb244cc8de979be5555dcf26634d1ee817d87
Jean28518 <jf_v@gmx.de>
2021-08-31 19:17:32 +0200

    Fixed caching function in jSaveModule

--- addons/jean28518.jTools/jSaveManager/jSaveModule.gd
+++ addons/jean28518.jTools/jSaveManager/jSaveModule.gd
@@ -9,8 +9,8 @@ func set_save_path(save_path : String):
 	_load_current_config()
 
 func save_value(key : String, value):
+	_value_changed = true
 	_cache_main[key] = value
-
 
 func get_value(key,  default_value = null):
 	if _cache_main.has(key):
@@ -25,6 +25,7 @@ func get_value(key,  default_value = null):
 	return default_value
 
 func reload():
+	_invalidate_cache()
 	_load_current_config()
 
 func write_to_disk():
@@ -34,16 +35,20 @@ func write_to_disk():
 	for key in _cache_main.keys():
 		_config.set_value("Main", key, _cache_main[key])
 	_config.save(save_path)
-	
+
 func load_everything_into_cache():
 	reload()
 	for key in _config.get_section_keys("Main"):
 		_cache_main[key] = _config.get_value("Main", key, null)
-	
+
 
 ## Internal Code ###############################################################
 var _config
 var _cache_main = {}
+var _value_changed = false
+
+func _invalidate_cache():
+	_cache_main = {}
 
 func _ready():
 	_load_current_config()
@@ -65,4 +70,5 @@ func _enter_tree():
 
 
 func _exit_tree():
-	write_to_disk()
+	if _value_changed:
+		write_to_disk()
